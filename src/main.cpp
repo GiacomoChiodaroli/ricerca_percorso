744eb5c0f87be4cef81aa7c55d5731b7207b3dfb
Giacomo Chiodaroli <giacomo.chiodaroli@edu.unifi.it>
2026-01-09 15:30:35 +0100

    commenti main

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,21 @@
 #include "map.h"
 #include "pxcount.h"
 
+int numcellsX=20,numcellsY=20;                  //dimensioni mappa, possono essere cambiate senza problemi
+
 int main() {
-    int mouseX=0, mouseY=0;
-    bool canmove=false;
-    int numcellsX=32,numcellsY=18;
     srand(time(0));
+
+    bool canmove=false;                         //verrà usato per decidere se muovere o meno il personaggio
+
     map mappa(numcellsX,numcellsY);
-    int randX=rand() % 32, randY=rand() % 18;
+    int randX=rand() % 32, randY=rand() % 18;  //verrà usato per decidere dove spawna il personaggio
+
     auto window = sf::RenderWindow( sf::VideoMode::getDesktopMode(),"mytab");
     window.setFramerateLimit(144);
+
     pxcount px(numcellsX,numcellsY,window.getSize().x,window.getSize().y);
+
     while (window.isOpen()) {
         px.setPx(numcellsX,numcellsY,window.getSize().x,window.getSize().y);
         while (const std::optional event = window.pollEvent()) {
@@ -26,12 +31,12 @@ int main() {
             }
             if (const auto* resized = event->getIf<sf::Event::Resized>()) {
                 sf::FloatRect areaVisibile({0.f, 0.f}, {static_cast<float>(resized->size.x), static_cast<float>(resized->size.y)});
-                window.setView(sf::View(areaVisibile));
+                window.setView(sf::View(areaVisibile));             //resize della finestra
             }
             if (const auto* mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>()) {
                 if (mouseButtonPressed->button == sf::Mouse::Button::Right) {
-                    mouseX=floor(mouseButtonPressed->position.x/px.getPxWidth());
-                    mouseY=floor(mouseButtonPressed->position.y/px.getPxHeight());
+                    int mouseX=floor(mouseButtonPressed->position.x/px.getPxWidth());
+                    int mouseY=floor(mouseButtonPressed->position.y/px.getPxHeight());
                     std::cout << "x: " << mouseX << ", y: " << mouseY << std::endl;
                 }
             }
